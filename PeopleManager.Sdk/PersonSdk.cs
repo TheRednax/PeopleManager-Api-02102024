6e67d4a20da20b678cb95e43a0512d654daaea12
TheRednax <136997904+TheRednax@users.noreply.github.com>
2024-10-02 17:22:44 +0200

    Initial commit

---
+++ PeopleManager.Sdk/PersonSdk.cs
@@ -0,0 +1,109 @@
+﻿using System.Net.Http.Json;
+using PeopleManager.Dto.Requests;
+using PeopleManager.Dto.Results;
+using Vives.Services.Model;
+using Vives.Services.Model.Extensions;
+
+namespace PeopleManager.Sdk
+{
+    public class PersonSdk(IHttpClientFactory httpClientFactory)
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+
+        //Find
+        public async Task<IList<PersonResult>> Find()
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+
+            var route = "People";
+            var response = await httpClient.GetAsync(route);
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<IList<PersonResult>>();
+            if (result is null)
+            {
+                return new List<PersonResult>();
+            }
+
+            return result;
+        }
+
+        //Get
+        public async Task<ServiceResult<PersonResult>> Get(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+
+            var route = $"People/{id}";
+            var response = await httpClient.GetAsync(route);
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<ServiceResult<PersonResult>>();
+            if (result is null)
+            {
+                return new ServiceResult<PersonResult>();
+            }
+
+            return result;
+        }
+
+        //Create
+        public async Task<ServiceResult<PersonResult>> Create(PersonRequest request)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+
+            var route = "People";
+            var response = await httpClient.PostAsJsonAsync(route, request);
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<ServiceResult<PersonResult>>();
+            if (result is null)
+            {
+                return new ServiceResult<PersonResult>();
+            }
+
+            return result;
+        }
+
+        //Update
+        public async Task<ServiceResult<PersonResult>> Update(int id, PersonRequest request)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+
+            var route = $"People/{id}";
+            var response = await httpClient.PutAsJsonAsync(route, request);
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<ServiceResult<PersonResult>>();
+            if (result is null)
+            {
+                result = new ServiceResult<PersonResult>();
+                result.NotFound(nameof(PersonResult), id);
+            }
+
+            return result;
+        }
+
+        //Delete
+        public async Task<ServiceResult<PersonResult>> Delete(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("PeopleManagerApi");
+
+            var route = $"People/{id}";
+            var response = await httpClient.DeleteAsync(route);
+
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<ServiceResult<PersonResult>>();
+            if (result is null)
+            {
+                return new ServiceResult<PersonResult>();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PeopleManager.Ui.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PeopleManager.Sdk;
+using PeopleManager.Sdk.Extensions;
+using PeopleManager.Ui.ConsoleApp.Settings;
+
+var configurationBuilder = new ConfigurationBuilder();
+var services = new ServiceCollection();
+
+configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+var configuration = configurationBuilder.Build();
+
+var apiSettings = new ApiSettings();
+configuration.GetSection(nameof(ApiSettings)).Bind(apiSettings);
+services.AddApi(apiSettings.BaseUrl);
+
+var serviceProvider = services.BuildServiceProvider();
+
+//Show Organizations
+var organizationService = serviceProvider.GetRequiredService<OrganizationSdk>();
+
+var organizations = await organizationService.Find();
+
+foreach (var organization in organizations)
+{
+    Console.WriteLine($"[{organization.Id}] {organization.Name} ({organization.NumberOfMembers} members)");
+}
+
+//Show People
+var personService = serviceProvider.GetRequiredService<PersonSdk>();
+
+var people = await personService.Find();
+
+foreach (var person in people)
+{
+    Console.WriteLine($"[{person.Id}] {person.FirstName} {person.LastName} ({person.OrganizationName})");
+}
+
+Console.ReadLine();
